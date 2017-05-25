984ce9e77097f64e4e278202cf566ed1634a6169
Your Name <you@example.comclear>
2017-05-25 21:52:18 +0530

    compleated boyer moor

--- BM/last.cpp
+++ BM/last.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include <string.h>
-class Needle{
-
+class BoyerMoorMatcher{
 	char* needle;
+	char* hayStack;
 	int hash [128];
-	public:
-		Needle(char str[]){
-			needle=str;
-		}
 
-		void Needle::calculateLast(){
+	void calculateLast(){
 			for(int i=0;i<128;i++){
 				hash[i]=-1;
 			}
@@ -18,22 +14,67 @@ class Needle{
 			}
 		}
 
+	public:
+		void setNeedle(char * n){
+			needle=n;
+			calculateLast();			
+		}
+		
+		void setHayStack(char * h){
+			hayStack=h;			
+		}
+
 		int last(char a){
 			return hash[int(a)];
 		}
 
-		void test(Needle N,char c){
 
-			std::cout<<"\nneedle : \'"<<(N.needle)<<"\'";
-			std::cout<<"\nchar : "<<c;
-			std::cout<<"\nthe last index of \'"<< c << "\' :"<<N.last(c)<<"\n";
+		int match (){
+			int n = strlen(needle);
+			int h = strlen(hayStack);
+			int j=n-1;
+			int k=n-1;
+			int l;
+			std::cout<<h<<"the size of hayStack\n";
+			while(j<h){
+				std::cout<<"hayStack: "<<j<<" "<<hayStack[j]<<" needle: "<<k<<" "<<needle[k]<<"\n";
+
+				while (hayStack[j]==needle[k]){
+					std::cout<<"matching hayStack: "<<hayStack[j]<<" needle: "<<needle[k]<<"\n";
+					k-=1;
+					if (k<0){
+						return j;
+					}
+					j-=1;
+				}
+				l=last(hayStack[j]);
+				if (l<k){
+					std::cout<<"j:"<<j<<"	k:"<<k<<"	l:"<<l<<"	before \n ";
+					j=j+n-l-1;
+					k=n-1;
+					std::cout<<"j:"<<j<<"	k:"<<k<<"	l:"<<l<<"	after\n";
+				}
+				else{
+					j=j+n-k+1;
+					k=n-1;
+				}
+
+
+			}
+			return -15001504;
+
 		}
 
-}
+};
 
 
 
 int main(){
+	BoyerMoorMatcher bm;
+	bm.setNeedle("abacab");
+	bm.setHayStack("abacaabadcabacab");
+	std::cout<<bm.match()<<"\n";
+
 
 }
 
